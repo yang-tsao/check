78ab3df66c07520ab12fdbc5dbb66a788948d02b
12vpn <caoyang2005@outlook.com>
2021-04-28 17:53:20 +0800

    φ
    φ

---
+++ φ.py
@@ -0,0 +1,19 @@
+import math
+import bisect
+ac = '.`^,:;!i><@+/-?][}{1)(|/tfjrxnuvczXYUJCLQ0OZmwqpdbkhao*#MW&%$'
+n = int(input())
+l = [list(map(float, input().split())) for i in range(n)]
+x, y = map(int, input().split())
+m = [[0]*y for i in range(x)]
+L = []
+for i in range(x-1, -1, -1):
+    for j in range(y):
+        for cg in l:
+            m[i][j] += cg[2]/math.sqrt((i-cg[1])**2+(j-cg[0])**2)
+        m[i][j] = abs(m[i][j])
+        L.append(m[i][j])
+L.sort()
+for i in range(x-1, -1, -1):
+    for j in range(y):
+        print(ac[int(bisect.bisect_left(L, m[i][j])/len(L)*len(ac))], end='')
+    print()
